b755d09828593d52c4ef43f6f55b82610e516350
mahad bin aamir <26100249@lums.edu.pk>
2026-02-16 19:36:37 +0500

    done

--- src/header.jsx
+++ src/header.jsx
@@ -1,24 +1,12 @@
 import React from "react";
+import logo from "./assets/Image_fx.png";
 
 const Header = () => {
   return (
     <header className="w-full px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto flex items-center justify-between py-4 border-b border-primary/20 dark:border-primary/30">
         <div className="flex items-center gap-3">
-          <div className="w-8 h-8 text-primary">
-            <svg
-              fill="none"
-              viewBox="0 0 48 48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clipRule="evenodd"
-                d="M12.0799 24L4 19.2479L9.95537 8.75216L18.04 13.4961L18.0446 4H29.9554L29.96 13.4961L38.0446 8.75216L44 19.2479L35.92 24L44 28.7521L38.0446 39.2479L29.96 34.5039L29.9554 44H18.0446L18.04 34.5039L9.95537 39.2479L4 28.7521L12.0799 24Z"
-                fill="currentColor"
-                fillRule="evenodd"
-              ></path>
-            </svg>
-          </div>
+          <img className="w-8 h-8" src={logo} alt="AgriPay" />
           <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
             AgriPay
           </h1>
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 // Import useLocation along with Link
 import { Link, useLocation } from "react-router-dom";
 import "./navbar.css";
+import logo from "./assets/Image_fx.png";
 
 const Navbar = () => {
   const location = useLocation();
@@ -11,21 +12,8 @@ const Navbar = () => {
     <header className="header">
       <div className="header-content">
         <Link to="/" className="logo-container">
-          <div className="logo-svg">
-            <svg
-              fill="none"
-              viewBox="0 0 48 48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clipRule="evenodd"
-                d="M12.0799 24L4 19.2479L9.95537 8.75216L18.04 13.4961L18.0446 4H29.9554L29.96 13.4961L38.0446 8.75216L44 19.2479L35.92 24L44 28.7521L38.0446 39.2479L29.96 34.5039L29.9554 44H18.0446L18.04 34.5039L9.95537 39.2479L4 28.7521L12.0799 24Z"
-                fill="currentColor"
-                fillRule="evenodd"
-              ></path>
-            </svg>
-          </div>
-          <h1 className="logo-text">AgroPay</h1>
+          <img className="logo-img" src={logo} alt="AgriPay" />
+          <h1 className="logo-text">AgriPay</h1>
         </Link>
         <nav className="nav">
           <a className="nav-link" href="#">
